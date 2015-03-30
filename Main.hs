7ea0ae08437f0580c664401d3c91f159d7926de6
Daniel Choi <dhchoi@gmail.com>
2015-03-29 22:49:37 -0400

    This strips the h1 tag and replces it with the text
    The withNav has no effect

---
+++ Main.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE OverloadedStrings, RecordWildCards #-} 
+module Main where
+
+import Text.XML.HXT.Core
+
+main = do
+  runX (readDocument [ withValidate no, withParseHTML yes, withInputEncoding utf8 ] ""
+        >>> processTopDown process1
+        >>> writeDocument [ ] "-" 
+        )
+
+process1 :: ArrowXml a => a XmlTree XmlTree
+process1 = 
+      (getChildren
+       <+>
+        withNav (
+          followingSiblingAxis >>> filterAxis (hasName "p")
+          >>> none
+        )
+      )
+      `when` (hasName "h1")
+        
+
+{-
+process = deep (isElem >>> hasName "p")
+          >>> withNav (followingSiblingAxis >>> filterAxis (isElem >>> hasName "p"))
+ -}         
